afab679cbeb1d5e3ece1587ae3d4b2ca4ff7ff9d
Momoe <momoemothana@gmail.com>
2025-05-29 15:52:32 +0700

    Modal popup ของหน้า sign in sign up

--- backend/momoeServer.js
+++ backend/momoeServer.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const PORT = 1234567890; // <-- มาเปลี่ยนพอร์ตตรงนี้นะจ๊ะ
+const PORT = 8000; // <-- มาเปลี่ยนพอร์ตตรงนี้นะจ๊ะ
 
 app.use(cors());
 app.use(bodyParser.json());
