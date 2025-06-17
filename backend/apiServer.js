e697055359185698b42b89a8658d6b8d62e7af83
Momoe <momoemothana@gmail.com>
2025-06-17 21:12:25 +0700

    แก้ไขให้รูปในหน้าแอดมินขึ้น

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-
+const path = require('path');
 const orderRoutes = require('./routes/orderRoutes');
 
 app.use(cors());
@@ -13,7 +13,8 @@ app.use(bodyParser.json());
 app.use('/api/account', require('./routes/account'));
 app.use('/api/country', require('./routes/country'));
 app.use('/api/products', require('./routes/products'));
-app.use('/api/order', orderRoutes); // ✅ ตรงนี้คือที่ใช้ได้
+app.use('/api/order', orderRoutes); 
+app.use('/img', express.static(path.join(__dirname, '..', 'img')));
 
 app.listen(PORT, () => {
   console.log('✅ Server running at http://localhost:' + PORT);
